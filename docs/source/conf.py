829c28149f283e4844c4b522f0d17d2802336137
peter-sas <peter.zhang@sas.com>
2019-11-18 14:28:48 -0500

    minor update on sphinx-rtd-theme setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,6 +31,7 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,12 +50,7 @@ exclude_patterns = []
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
